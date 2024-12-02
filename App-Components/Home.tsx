5bef596eff3e20125f0ed0b361488dec30a5d06a
Lisha Ranganath <lishar2002@gmail.com>
2024-12-02 21:15:15 +0530

    styling changed

--- App-Components/Home.tsx
+++ App-Components/Home.tsx
@@ -1,22 +1,48 @@
-import { Pressable,  Image , Text, View} from "react-native";
+import { Pressable,  Image , Text, View, StyleSheet} from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome'; // Import FontAwesome icons
 
 const locImage = require("../assets/images/img1.jpeg")
 export default function Home(){
     return(
-        <View style={{flex:1}}>
-            <Image source={locImage}></Image>
-            <Text style = {{ textAlign: "center", margin:10 , fontSize: 30}}> Home</Text>
-            <Icon name="home" size={40} color="black" style={{ textAlign: "center"}}/>
-           <Pressable style={{flex: 100, backgroundColor: "white", margin:20}} onPress={()=> console.log("Profile is pressed")}>
-                <Text style={{textAlign: "center", fontSize: 30}}>Profile</Text>
+        <View style={styles.container}>
+            <Image source={locImage} style = {styles.image}></Image>
+            <Text style = {{ textAlign: "center", margin:10 , fontSize: 55}}> Home</Text>
+            <Icon name="home" size={60} color="black" style={{ textAlign: "center", paddingBottom: 20}}/>
+           <Pressable style={styles.action_buttons} onPress={()=> console.log("Profile is pressed")}>
+                <Text style={styles.text_styles}>Profile</Text>
             </Pressable>
-            <Pressable style={{flex: 100, backgroundColor: "white", margin:20}} onPress={()=> console.log("Settings is pressed")}>
-                <Text style={{textAlign: "center", fontSize: 30}}>Setting</Text>
+            <Pressable style={styles.action_buttons} onPress={()=> console.log("Settings is pressed")}>
+                <Text style={styles.text_styles}>Setting</Text>
             </Pressable>
-            <Pressable style={{flex: 100, backgroundColor: "white", margin:20}} onPress={()=> console.log("Logout is pressed")}>
-                <Text style={{textAlign: "center", fontSize: 30}}>Logout</Text>
+            <Pressable style={styles.action_buttons} onPress={()=> console.log("Logout is pressed")}>
+                <Text style={styles.text_styles}>Logout</Text>
             </Pressable>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "plum"
+    },
+    image:{
+        borderRadius : 20,
+        marginBottom: 50,
+    },
+    action_buttons:{
+        flex:100,
+        backgroundColor: "white",
+        margin:20,
+        borderRadius: 10,
+        width: 250,
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    text_styles:{
+        textAlign: "center",
+        fontSize: 30
+    }
+})
